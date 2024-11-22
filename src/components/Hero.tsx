f026f325741aed755f3a14bc58e5d5d9bb03bafe
Haneul <77052813+ohaneul@users.noreply.github.com>
2024-11-22 21:00:34 +0100

    Hero Image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,7 +17,7 @@ export default function Hero() {
       className="relative h-screen w-full bg-cover bg-center bg-fixed"
       style={{
         backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), 
-        url('https://images.unsplash.com/photo-1723572206884-cb2c3a7f99bf?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBvcnNjaGUlMjA5OTIlMjBndDN8ZW58MHx8MHx8fDA%3D')`
+        url('https://newsroom.porsche.com/.imaging/mte/porsche-templating-theme/image_1080x624/dam/pnr/2020/motorsports/2020/Stronger,-faster,-more-spectacular--the-new-911-GT3-Cup/SP21L31OX0007_Presse.jpeg/jcr:content/SP21L31OX0007_Presse.jpeg')`
       }}
     >
       <div className="absolute inset-0 bg-gradient-to-br from-pink-500/30 via-transparent to-purple-900/50" />
